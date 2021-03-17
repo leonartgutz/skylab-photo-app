1131348080ee5ee64cf6acbe597dd40eea8a91c0
Leonart Gutz <leonartgutz@hotmail.com>
2021-03-17 01:34:25 -0700

    Home Function Done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,61 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import { Platform, StyleSheet } from 'react-native'
 import { InfoMessage, PageTitleText, HomeBody, PhotoExampleImage, PhotoExampleWrap, SelectPhotoBtn, SelectPhotoBtnText, AgreementText } from './homeStyles'
 const PhotoExample = require('../../../assets/photo-example.png')
 
-const Home: React.FC = () => (
-  <HomeBody>
-    <PageTitleText>Professional retouching.</PageTitleText>
-    <PageTitleText>Powered by A.I.</PageTitleText>
-    
-    <PhotoExampleWrap style={boxShadow.properties}>
-      <PhotoExampleImage source={PhotoExample} />
-    </PhotoExampleWrap>
+interface Props {
+  history: any
+}
 
-    <InfoMessage>Select a photo to start</InfoMessage>
+const Home: React.FC<Props> = ({ history }) => {
 
-    <SelectPhotoBtn onPress={() => {}} style={boxShadow.properties}>
-      <SelectPhotoBtnText>Select Photo</SelectPhotoBtnText>
-    </SelectPhotoBtn>
+  // Show Album
+  const showAlbum = async (): Promise<void> => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-    <AgreementText>by selecting a photo you agree to our Terms of Service and Privacy Policy</AgreementText>
-  </HomeBody>
-)
+    if (!result.cancelled) {
+      history.push('/album')
+    }
+  }
+
+  // Check for permission
+  const checkPermission = async (): Promise<void> => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+
+      } else {
+        showAlbum();
+
+      }
+    }
+  };
+
+  return <HomeBody>
+      <PageTitleText>Professional retouching.</PageTitleText>
+      <PageTitleText>Powered by A.I.</PageTitleText>
+      
+      <PhotoExampleWrap style={boxShadow.properties}>
+        <PhotoExampleImage source={PhotoExample} />
+      </PhotoExampleWrap>
+
+      <InfoMessage>Select a photo to start</InfoMessage>
+
+      <SelectPhotoBtn onPress={checkPermission} style={boxShadow.properties}>
+        <SelectPhotoBtnText>Select Photo</SelectPhotoBtnText>
+      </SelectPhotoBtn>
+
+      <AgreementText>by selecting a photo you agree to our Terms of Service and Privacy Policy</AgreementText>
+    </HomeBody>
+}
 
 // Styling for box-shadow
 const boxShadow = StyleSheet.create({
